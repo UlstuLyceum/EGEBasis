9b8d44b4b7734611ab5f7be9d883dbd68bb3790b
Ramil Suleymanov <lllumineux@outlook.com>
2020-02-04 20:35:34 +0400

    Add main menu and header

--- src/__main__.py
+++ src/__main__.py
@@ -18,4 +18,21 @@ def signup():
     return render_template("signup.html", title="Зарегистрироваться")
 
 
+@app.route("/<subj_name>/<mode_name>")
+def app_logged_in(subj_name, mode_name):
+    if mode_name == "tasks":
+        return render_template("tasks.html", title="Задания",
+                               header_label="Все задания первой части",
+                               current_subj=subj_name,
+                               current_mode=mode_name,
+                               subject_list=[
+                                   {"label": "Русский",
+                                    "name": "russian"},
+                                   {"label": "Математика",
+                                    "name": "math"},
+                                   {"label": "Информатика",
+                                    "name": "it"}
+                               ])
+
+
 app.run(port=8000)
