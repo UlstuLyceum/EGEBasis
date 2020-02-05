1ba7c5c8439272ed5787cd46967e81fde72337d4
Egor Sbrodov <egor-ulstu@yandex.ru>
2020-02-05 18:36:17 +0400

    Tasks list added

--- src/init.py
+++ src/init.py
@@ -23,44 +23,10 @@ app.config["MAIL_PASSWORD"] = config.MAIL_PASSWORD
 mail = Mail(app)
 
 from src.modules.auth_module.auth import auth
+from src.modules.tasks_module.tasks import tasks
 
 app.register_blueprint(auth)
-
-
-subject_list = [
-    {"label": "Русский", "name": "russian"},
-    {"label": "Математика", "name": "math"},
-    {"label": "Информатика", "name": "it"},
-]
-
-tasks_list = [
-    {"number": 1, "compete_percentage": 100, "points": 1, "status": 3},
-    {"number": 2, "compete_percentage": 47, "points": 1, "status": 1},
-    {"number": 3, "compete_percentage": 70, "points": 1, "status": 1},
-    {"number": 4, "compete_percentage": 34, "points": 1, "status": 2},
-    {"number": 5, "compete_percentage": 28, "points": 1, "status": 1},
-    {"number": 6, "compete_percentage": 82, "points": 1, "status": 3},
-    {"number": 7, "compete_percentage": 56, "points": 1, "status": 3},
-    {"number": 8, "compete_percentage": 27, "points": 5, "status": 1},
-    {"number": 9, "compete_percentage": 72, "points": 1, "status": 3},
-    {"number": 10, "compete_percentage": 64, "points": 1, "status": 2},
-    {"number": 11, "compete_percentage": 25, "points": 1, "status": 1},
-    {"number": 12, "compete_percentage": 46, "points": 1, "status": 0},
-    {"number": 13, "compete_percentage": 35, "points": 1, "status": 1},
-    {"number": 14, "compete_percentage": 67, "points": 1, "status": 2},
-    {"number": 15, "compete_percentage": 43, "points": 1, "status": 2},
-    {"number": 16, "compete_percentage": 37, "points": 2, "status": 1},
-    {"number": 17, "compete_percentage": 96, "points": 1, "status": 2},
-    {"number": 18, "compete_percentage": 66, "points": 1, "status": 3},
-    {"number": 19, "compete_percentage": 24, "points": 1, "status": 1},
-    {"number": 20, "compete_percentage": 63, "points": 1, "status": 0},
-    {"number": 21, "compete_percentage": 67, "points": 1, "status": 1},
-    {"number": 22, "compete_percentage": 35, "points": 1, "status": 2},
-    {"number": 23, "compete_percentage": 74, "points": 1, "status": 3},
-    {"number": 24, "compete_percentage": 35, "points": 1, "status": 0},
-    {"number": 25, "compete_percentage": 26, "points": 1, "status": 1},
-    {"number": 26, "compete_percentage": 27, "points": 4, "status": 0},
-]
+app.register_blueprint(tasks)
 
 
 @app.route("/")
@@ -68,18 +34,5 @@ def index():
     user = get_current_user()
     if user is None:
         return redirect(url_for("auth.login"))
-    return redirect(url_for("app_logged_in", subj_name="russian", mode_name="tasks"))
+    return redirect(url_for("tasks.app_logged_in", subj_name="russian", mode_name="tasks"))
 
-
-@app.route("/<subj_name>/<mode_name>")
-def app_logged_in(subj_name, mode_name):
-    if mode_name == "tasks":
-        return render_template(
-            "tasks.html",
-            title="Задания",
-            header_label="Все задания первой части",
-            current_subj=subj_name,
-            current_mode=mode_name,
-            subject_list=subject_list,
-            tasks_list=tasks_list,
-        )
--- src/lib.py
+++ src/lib.py
@@ -2,7 +2,7 @@ import hashlib
 
 from flask import render_template, session
 
-from src.models import User
+from src.models import User, TaskLink, Task, TaskTypeLink
 
 try:
     import src.local_config as config
@@ -29,3 +29,25 @@ def hash_password(raw_data):
 
 def generate_confirm_code(email):
     return hash_password(email)[:10]
+
+
+def count_percentage_on_task(tasktype):
+    user = get_current_user()
+    related_tasks = Task.find({"task_type": tasktype.id})
+    done_tasks = 0
+    for task in related_tasks:
+        done_tasks += TaskLink.find({"done": True, "task": task.id, "user": user.id}).count()
+    all_tasks = related_tasks.count()
+    if all_tasks == 0:
+        print("No tasks found for task type " + tasktype.number)
+        return 0
+    print(done_tasks, all_tasks)
+    return int(done_tasks / all_tasks * 100)
+
+
+def get_status_on_task(tasktype):
+    user = get_current_user()
+    res = TaskTypeLink.find_one({"task_type": tasktype.id, "user": user.id})
+    if not res:
+        return 0
+    return res.status
