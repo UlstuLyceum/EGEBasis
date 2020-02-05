b60fa2d0a31f5b90d733ffee40fcf7ae8e9b3905
Egor Sbrodov <egor-ulstu@yandex.ru>
2020-02-05 22:07:45 +0400

    Tasks added

--- src/modules/tasks_module/tasks.py
+++ src/modules/tasks_module/tasks.py
@@ -1,13 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
-from src.lib import count_percentage_on_task, get_status_on_task, render
-from src.models import Subject, TaskType
+from src.lib import count_percentage_on_task, get_status_on_task, render, get_current_user
+from src.models import Subject, TaskType, Task
 
 tasks = Blueprint("tasks", __name__, template_folder="templates")
 
 
 @tasks.route("/<subj_name>/<mode_name>")
 def app_logged_in(subj_name, mode_name):
+    if get_current_user() is None:
+        return redirect(url_for("index"))
     if mode_name == "tasks":
         subject_list = list(Subject.find({"hidden": False}))
         subject = Subject.find_one({"name": subj_name})
@@ -32,3 +35,27 @@ def app_logged_in(subj_name, mode_name):
             tasks_list=tasks_list,
         )
     return "No such page"
+
+
+@tasks.route('/<subj_name>/task/<int:task_id>')
+def task_theory(subj_name, task_id):
+    return render("task-theory.html", current_subj=subj_name, task_id=task_id)
+
+
+@tasks.route('/<subj_name>/task/<int:task_id>/tasks')
+def task_tasks(subj_name, task_id):
+    if get_current_user() is None:
+        return redirect(url_for("index"))
+    user = get_current_user()
+    subject = Subject.find_one({"name": subj_name})
+    task_type = TaskType.find_one({"subject": subject.id, "number": str(task_id)})
+    tasks = []
+    raw_tasks = Task.find({"task_type": task_type.id})
+    for task in raw_tasks:
+        tasks.append({
+            "body": "Тут содержание задания",
+            "text": None,  # связанный с заданием текст
+            "done": True,
+            "answer": "123"
+        })
+    return render("task-list.html", current_subj=subj_name, tasks=tasks)
