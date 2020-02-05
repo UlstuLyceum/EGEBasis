301799bfd25e2706b10af57905a3e8c9b8782351
Ramil Suleymanov <lllumineux@outlook.com>
2020-02-05 14:47:47 +0400

    Add task page

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,180 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+subject_list = [
+    {
+        "label": "Русский",
+        "name": "russian"
+    },
+    {
+        "label": "Математика",
+        "name": "math"
+    },
+    {
+        "label": "Информатика",
+        "name": "it"
+    }
+]
+
+tasks_list = [
+    {
+        "number": 1,
+        "compete_percentage": 100,
+        "points": 1,
+        "status": 3
+    },
+    {
+        "number": 2,
+        "compete_percentage": 47,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 3,
+        "compete_percentage": 70,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 4,
+        "compete_percentage": 34,
+        "points": 1,
+        "status": 2
+    },
+    {
+        "number": 5,
+        "compete_percentage": 28,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 6,
+        "compete_percentage": 82,
+        "points": 1,
+        "status": 3
+    },
+    {
+        "number": 7,
+        "compete_percentage": 56,
+        "points": 1,
+        "status": 3
+    },
+    {
+        "number": 8,
+        "compete_percentage": 27,
+        "points": 5,
+        "status": 1
+    },
+    {
+        "number": 9,
+        "compete_percentage": 72,
+        "points": 1,
+        "status": 3
+    },
+    {
+        "number": 10,
+        "compete_percentage": 64,
+        "points": 1,
+        "status": 2
+    },
+    {
+        "number": 11,
+        "compete_percentage": 25,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 12,
+        "compete_percentage": 46,
+        "points": 1,
+        "status": 0
+    },
+    {
+        "number": 13,
+        "compete_percentage": 35,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 14,
+        "compete_percentage": 67,
+        "points": 1,
+        "status": 2
+    },
+    {
+        "number": 15,
+        "compete_percentage": 43,
+        "points": 1,
+        "status": 2
+    },
+    {
+        "number": 16,
+        "compete_percentage": 37,
+        "points": 2,
+        "status": 1
+    },
+    {
+        "number": 17,
+        "compete_percentage": 96,
+        "points": 1,
+        "status": 2
+    },
+    {
+        "number": 18,
+        "compete_percentage": 66,
+        "points": 1,
+        "status": 3
+    },
+    {
+        "number": 19,
+        "compete_percentage": 24,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 20,
+        "compete_percentage": 63,
+        "points": 1,
+        "status": 0
+    },
+    {
+        "number": 21,
+        "compete_percentage": 67,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 22,
+        "compete_percentage": 35,
+        "points": 1,
+        "status": 2
+    },
+    {
+        "number": 23,
+        "compete_percentage": 74,
+        "points": 1,
+        "status": 3
+    },
+    {
+        "number": 24,
+        "compete_percentage": 35,
+        "points": 1,
+        "status": 0
+    },
+    {
+        "number": 25,
+        "compete_percentage": 26,
+        "points": 1,
+        "status": 1
+    },
+    {
+        "number": 26,
+        "compete_percentage": 27,
+        "points": 4,
+        "status": 0
+    },
+]
+
 
 @app.route("/")
 def index():
@@ -25,14 +199,8 @@ def app_logged_in(subj_name, mode_name):
                                header_label="Все задания первой части",
                                current_subj=subj_name,
                                current_mode=mode_name,
-                               subject_list=[
-                                   {"label": "Русский",
-                                    "name": "russian"},
-                                   {"label": "Математика",
-                                    "name": "math"},
-                                   {"label": "Информатика",
-                                    "name": "it"}
-                               ])
+                               subject_list=subject_list,
+                               tasks_list=tasks_list)
 
 
 app.run(port=8000)
